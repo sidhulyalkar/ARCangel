8a45c7aadc9eceefca5abc2485aa686bfe3545b1
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-22 15:11:18 -0700

    V005: export predictive memory primitives

--- src/arc3lab/memory/__init__.py
+++ src/arc3lab/memory/__init__.py
@@ -1,3 +1,9 @@
 from .episode import EpisodeMemory
+from .predictive import PredictiveTransitionMemory, action_key, predictive_state_key
 
-__all__ = ["EpisodeMemory"]
+__all__ = [
+    "EpisodeMemory",
+    "PredictiveTransitionMemory",
+    "action_key",
+    "predictive_state_key",
+]
