74dc0c0a68697ecc7a7fb714dc484b6f686cb2f8
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-24 19:30:43 -0700

    V010: export ActiveScientistPolicy

--- src/arc3lab/policy/__init__.py
+++ src/arc3lab/policy/__init__.py
@@ -1,19 +1,21 @@
+from arc3lab.policy.active_scientist import ActiveScientistPolicy
 from arc3lab.policy.coding import CodingPolicy
 from arc3lab.policy.effect_posterior import EffectPosteriorPolicy
 from arc3lab.policy.hybrid import HybridPolicy
+from arc3lab.policy.perceptual_coding import PerceptualDecisionPolicy
 from arc3lab.policy.random_policy import RandomPolicy
 from arc3lab.policy.spatial_coding import SpatialCodingPolicy
 from arc3lab.policy.structural import StructuralPolicy
 from arc3lab.policy.visual_coding import VisualDecisionPolicy
-from arc3lab.policy.perceptual_coding import PerceptualDecisionPolicy
 
 __all__ = [
+    "ActiveScientistPolicy",
     "CodingPolicy",
     "EffectPosteriorPolicy",
     "HybridPolicy",
+    "PerceptualDecisionPolicy",
     "RandomPolicy",
     "SpatialCodingPolicy",
     "StructuralPolicy",
     "VisualDecisionPolicy",
-    "PerceptualDecisionPolicy",
 ]
